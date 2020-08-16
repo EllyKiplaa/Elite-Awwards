00224062aac96f6f0b69e5a10a11175888a78458
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-17 00:27:09 +0300

    add the model class profile

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Models
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    avi=ImageField(manual_crop='')
+    bio=models.CharField(max_length=240)
+    phone=models.IntegerField(blank=True)
+    email=models.EmailField()
+
+
