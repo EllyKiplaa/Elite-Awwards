ebd61e177728560fce8f535982af69b405fff90d
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-17 13:05:47 +0300

    add forms that will get me upload photos and update

---
+++ awards/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Profile,Project,Review
+SCORES = [
+    (1,1),(2,2),
+    (3,3),(4,4),
+    (5,5),(6,6),
+    (7,7),(8,8),
+    (9,9),(10,10),
+]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['user']
+
+class ProjectUploadForm(forms.ModelForm):
+    class Meta:
+        model=Project
+        exclude=['user']  
