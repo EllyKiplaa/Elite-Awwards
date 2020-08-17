10e0a4beffd64b3469e63f8cbe88c13dcbbfa486
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-17 14:16:15 +0300

    add Class method review

--- awards/models.py
+++ awards/models.py
@@ -17,3 +17,32 @@ class Project(models.Model):
     description=models.TextField()
     live_site=models.URLField(max_length=299)
 
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    @classmethod
+    def get_all_projects(cls):
+        return cls.objects.all()
+
+    @classmethod
+    def single_project(cls,id):
+        return cls.objects.get(id=id)
+
+
+class Review(models.Model):
+    project=models.ForeignKey(Project,on_delete=models.CASCADE,related_name='review')
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='review')
+    comment=models.TextField()
+    design=models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    useability=models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    content=models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+
+    def __str__(self):
+        return self.user.username
+
+    @classmethod
+    def project_reviews(cls, id):
+        return cls.objects.filter(id)   
