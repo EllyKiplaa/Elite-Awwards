5ba7c009e6e54eb1f1fdf25ad38e8fa850a61147
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-17 11:10:16 +0300

    Add the urls to link to the profile search bar and profile

--- awards/urls.py
+++ awards/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import url
 
 urlpatterns=[
     url(r'^$', views.index, name='homepage'),
+    url(r'^profile/$', views.myProfile, name='profile'),
+    url(r'^search/$', views.search_title, name='search_title'),
+    url(r'^details/(\d+)$', views.details, name='details'),
+    url(r'^api/profile/$', views.Profile_list.as_view()),
+    url(r'^api/projects/$', views.Project_list.as_view()),
 
 ]
