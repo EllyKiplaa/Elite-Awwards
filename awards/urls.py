0d12e5742906b250e3d4f11a51be0e0fa048d2cc
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-18 00:52:23 +0300

    Import the static from django urls

--- awards/urls.py
+++ awards/urls.py
@@ -1,15 +1,19 @@
 from . import views
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView,  LogoutView
+from django.conf.urls.static import static
 
 
 
 urlpatterns=[
+    
     url(r'^$', views.index, name='homepage'),
     url(r'^profile/$', views.myProfile, name='profile'),
     url(r'^search/$', views.search_title, name='search_title'),
     url(r'^details/(\d+)$', views.details, name='details'),
     url(r'^api/profile/$', views.Profile_list.as_view()),
     url(r'^api/projects/$', views.Project_list.as_view()),
+
+
 
 ]
