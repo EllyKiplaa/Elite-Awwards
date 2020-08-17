5a75066917322c753b52576c4d594afb9ffecdf9
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-17 14:40:23 +0300

    add the such function

--- awards/views.py
+++ awards/views.py
@@ -36,4 +36,38 @@ def myProfile(request,**kwargs):
         'prof_update':prof_update,
         'user_posts':user_posts,
     }
-    return render(request, 'profile.html', locals()) 
+    return render(request, 'profile.html', locals())
+
+def details(request, id):
+    current_site=Project.single_project(id)
+    current_user=request.user
+    proj_reviews=Review.objects.filter(project=current_site)
+    review_form=ReviewForm(request.POST)
+    if review_form.is_valid():
+        review=review_form.save(commit=False)
+        review.user=current_user
+        review.project=current_site
+        review.save()
+        return HttpResponseRedirect(request.path_info)
+    else:
+        review_form=ReviewForm()
+    context={
+        'current_user':current_user,
+        'current_site':current_site,
+        'review_form':review_form,
+        'proj_reviews':proj_reviews,
+    }
+    return render(request, 'details.html',locals())
+
+
+def search_title(request):
+    if request.method == "GET":
+        search_term=request.GET.get('search')
+        got_projects=Project.objects.filter(title__icontains=search_term)[::-1]
+        context ={
+            'got_projects':got_projects,
+        }
+        return render(request, 'results.html', locals())
+    else:
+        message="Looking for something, type it and hit search"
+        return render(request, 'results.html', {'message':message})
