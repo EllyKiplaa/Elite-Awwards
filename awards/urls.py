3d785e5be54b92ae40d27e4f7c61b317be622b6f
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-19 19:51:25 +0300

    add add the search that will aid in searching

--- awards/urls.py
+++ awards/urls.py
@@ -10,17 +10,16 @@ urlpatterns=[
     
     url(r'^$', views.index, name='homepage'),
     url('accounts/profile/$', views.myProfile, name='profile'),
-    url(r'^search/$', views.search_title, name='search_title'),
+    url('search/',views.search_project, name='search_results'),
     url(r'^details/(\d+)$', views.details, name='details'),
     url(r'^api/profile/$', views.Profile_list.as_view()),
     url(r'^api/projects/$', views.Project_list.as_view()),
+    url(r'^profile/$',views.profile_edit,name='edit_profile'),
+    # url('project/review/<project_id>',views.project_review,name='project_review'),
     url('accounts/login/',LoginView.as_view(redirect_authenticated_user=True,template_name='accounts/login.html'),name='login'),
     url('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    url('accounts/register/',views.register, name='register'),
    
-    # url('logout/',LogoutView.as_view(), name='logout'),
-
 
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
